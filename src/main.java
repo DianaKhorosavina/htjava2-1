a31392a45fe9bbedec7b4ca74b8b82b843a4e967
Diana Khorosavina <dkhorosavina@gmail.com>
2023-09-25 19:33:45 +0300

    initial commit

---
+++ src/main.java
@@ -0,0 +1,18 @@
+public class main {
+    public static void main(String[] args) {
+        int price = 30_000;
+        int mile = 20;
+        int bonus = price / mile;
+        System.out.println("За покупку билета вам начислено " + bonus + " миль");
+    }
+}
+
+
+
+
+
+
+
+
+
+
